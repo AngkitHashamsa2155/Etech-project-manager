59bf9df0b7436c398c823de13d74345182362eb6
Angkit <angkithashamsa21@gmail.com>
2022-03-10 00:16:51 +0530

    cahnge in packgae json

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 /**
  * Home Page of the Application
  * @return {JSX.Element}
  */
 const App: React.FC = () => {
   const [count, setCount] = useState<number>(0);
+  const counterNumber = (): void => {
+    setCount(count + 1);
+  };
   return (
-    <div className="App">
+    <div className=" text-left text-lg">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={() => setCount(count + 1)}>Count</button>
-        <div>count</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={counterNumber}>Count</button>
+        <div>{count}</div>
       </header>
     </div>
   );
